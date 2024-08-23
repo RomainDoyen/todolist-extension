eac8646a6ea0651b1b424d424ae961e1faf22d22
RomainDoyen <romain.doyen97@gmail.com>
2024-08-23 08:56:01 +0400

    Added window size style and modified notes icon

--- src/components/CardNotes.jsx
+++ src/components/CardNotes.jsx
@@ -1,12 +1,12 @@
 import './CardNotes.css';
 // import { FaNoteSticky } from "react-icons/fa6";
-import { CiStickyNote } from "react-icons/ci";
+import { FaStickyNote } from "react-icons/fa";
 
 export default function CardNotes() {
   return (
     <div className="card-notes">
         <div className="todo-item">
-          <h3><CiStickyNote />&nbsp;Titre de la note</h3>
+          <h3><FaStickyNote />&nbsp;Titre de la note</h3>
           <p>Description</p>
         </div>
     </div>
